b5aea52146f60f3962360b74a94c38974a8674d0
Bakamura <ra00297288@pucsp.edu.br>
2022-07-13 22:03:58 -0300

    Tutorial Level & Provisory Camera Script

---
+++ Assets/Code/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    [SerializeField] private Vector3 _followOffSet = Vector3.up;
+
+    [Header("Proxy")]
+
+    private Transform _playerTransf = null;
+
+    private void Start() {
+        _playerTransf = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update() {
+        transform.position = _playerTransf.position + _followOffSet; // PROVISORY
+    }
+
+}
