7bbc3f1167e216cb0cf11c39d5f30599b770af8f
NakaVostro <ra00297288@pucsp.edu.br>
2022-08-26 13:17:42 -0300

    Hard Button Trigger Done
    - Still iterating in actions

--- Assets/Code/Scripts/HardButton.cs
+++ Assets/Code/Scripts/HardButton.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class HardButton : MonoBehaviour {
 
-    private void OnCollisionEnter(Collision collision) {
-        if(collision.relativeVelocity.magnitude > 10) {
+    [SerializeField] private float _necessaryVelocity = 7f;
 
+    [Header("MoveObject")]
+
+    [SerializeField] private GameObject _objToMove = null;
+    [SerializeField] private Vector3 _objNewPos = Vector3.zero;
+    [SerializeField] private float _duration = 1.5f;
+
+    private void Awake() {
+        _necessaryVelocity *= _necessaryVelocity;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.relativeVelocity.sqrMagnitude > _necessaryVelocity) StartCoroutine(Activate());
+    }
+
+    private IEnumerator Activate() {
+        Vector3 startPos = _objToMove.transform.position;
+        float currentStep = 0;
+        while (currentStep < 1) {
+            currentStep += Time.deltaTime / _duration;
+            if (currentStep >= 1) currentStep = 1;
+            _objToMove.transform.position = Vector3.Lerp(startPos, _objNewPos, currentStep);
+
+            yield return null;
         }
     }
 
